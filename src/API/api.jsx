68f3db4e4e74810cfb381c79db66122915f4845e
MeoKool <truongsyquang1202@gmail.com>
2025-04-02 12:29:04 +0700

    Refactor API endpoints: update base URL and enhance order item retrieval with parameters

--- src/API/api.jsx
+++ src/API/api.jsx
@@ -1,19 +1,32 @@
 import instance, { customAxios, customAxiosAPI } from "./axios";
 
-export const BASE_URL = "https://vietsac.id.vn/pizza-service/";
+export const BASE_URL = "https://vietsac.id.vn/";
 
 export const SignUpAccount = (userData) => {
   return instance.post(`api/Accounts/SignUpUser`, userData);
 };
-export const GetOrderItems = () => {
-  return customAxios.get(
-    `api/order-items?IncludeProperties=OrderItemDetails.OptionItem&OrderItemStatus=Pending&TakeCount=1000`
-  );
+export const GetOrderItems = (params) => {
+  return customAxios.get(`api/order-items`, {
+    params: {
+      ...params,
+      OrderItemStatus: "Pending",
+      TakeCount: 1000,
+      SortBy: "startTime",
+      IncludeProperties: "OrderItemDetails",
+    },
+  });
 };
-export const GetOrderItemsServing = () => {
-  return customAxios.get(
-    `api/order-items?IncludeProperties=OrderItemDetails.OptionItem&OrderItemStatus=Serving&TakeCount=1000`
-  );
+
+export const GetOrderItemsServing = (params) => {
+  return customAxios.get(`api/order-items`, {
+    params: {
+      ...params,
+      OrderItemStatus: "Serving",
+      TakeCount: 1000,
+      SortBy: "startTime",
+      IncludeProperties: "OrderItemDetails",
+    },
+  });
 };
 export const UpdateStatusServing = ({ id }) => {
   return customAxiosAPI.put(`api/order-items/serving/${id}`, { id });
